c9257ea3854e9d1aa292b11bf188b7e006ca7d97
Albina Akhmatova <akhmatova.albino4ka@yandex.ru>
2025-05-23 14:47:34 +0500

    Создание нормальной архитектуры + Формирование сущностей для БД

---
+++ Planiro/Infrastructure/Data/Entities/TaskEntity.cs
@@ -0,0 +1,18 @@
+﻿namespace Planiro.Infrastructure.Data.Entities;
+
+public class TaskEntity
+{
+    public string? Title { get; set; }
+
+    public string? Description { get; set; }
+    
+    public string? State { get; set; }
+    
+    public DateTime? Deadline { get; set; }
+    
+    public bool IsApproved { get; set; } = false;
+    
+    public Guid UserId;
+    
+    public UserEntity? User { get; set; }
+}
---
+++ Planiro/Infrastructure/Data/Entities/TeamEntity.cs
@@ -0,0 +1,14 @@
+﻿namespace Planiro.Infrastructure.Data.Entities;
+
+public class TeamEntity
+{
+    public Guid Id { get; set; }
+    
+    public string? JoinCode { get; set; }
+
+    public ICollection<UserEntity>? Users { get; set; } 
+    
+    public Guid TeamleadId { get; set; }
+    
+    public UserEntity? TeamleadEntity { get; set; }
+}
