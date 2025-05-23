ac56dac1ee2146dc18aa6e90aeae0fba4609b079
novik <novikov.anton.serg@gmail.com>
2025-05-23 22:53:55 +0500

    Flex №1

--- Planiro/Domain/Entities/User.cs
+++ Planiro/Domain/Entities/User.cs
@@ -3,14 +3,19 @@
 public class User
 {
     public Guid Id { get; set; }
-    
-    public string? FirstName { get; set; }
-
-    public string? LastName { get; set; }
-
-    public string? UserName { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string UserName { get; set; }
 
     public ICollection<Guid>? Teams { get; set; }
 
     public ICollection<Guid>? Planners { get; set; }
+
+    public User(Guid id, string firstName, string lastName, string userName)
+    {
+        Id = id;
+        FirstName = firstName;
+        LastName = lastName;
+        UserName = userName;
+    }
 }
