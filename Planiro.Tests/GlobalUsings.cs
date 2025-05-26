5d9f8abcf924cf59094e373e7f0c31c441b2ee2c
PavelMartinelli <pav.shipitsyn@gmail.com>
2025-05-26 20:08:57 +0500

    Добавлены базовые тесты

---
+++ Planiro.Tests/GlobalUsings.cs
@@ -0,0 +1,7 @@
+global using Microsoft.VisualStudio.TestTools.UnitTesting;
+global using Moq;
+global using Planiro.Application.Services;
+global using Planiro.Domain.Entities;
+global using Planiro.Domain.IRepositories;
+global using System;
+global using System.Threading.Tasks;
---
+++ Planiro.Tests/PasswordHasherTests.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Planiro.Application.Services;
+
+[TestClass]
+public class PasswordHasherTests
+{
+    private readonly PasswordHasher _hasher = new PasswordHasher();
+
+    [TestMethod]
+    public void HashPassword_ShouldReturnNonEmptyString()
+    {
+        var hash = _hasher.HashPassword("password123");
+        Assert.IsFalse(string.IsNullOrEmpty(hash));
+    }
+
+    [TestMethod]
+    public void VerifyPassword_ShouldReturnTrueForValidPassword()
+    {
+        var password = "password123";
+        var hash = _hasher.HashPassword(password);
+        Assert.IsTrue(_hasher.VerifyPassword(password, hash));
+    }
+
+    [TestMethod]
+    public void VerifyPassword_ShouldReturnFalseForInvalidPassword()
+    {
+        var hash = _hasher.HashPassword("password123");
+        Assert.IsFalse(_hasher.VerifyPassword("wrongpassword", hash));
+    }
+}
