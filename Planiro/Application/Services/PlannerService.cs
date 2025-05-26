480f9ae14b75c62ac2bcab6c952f99bb61434f1d
Albina Akhmatova <akhmatova.albino4ka@yandex.ru>
2025-05-26 18:01:11 +0500

    Изменили сигнатуру метода из репозитория

--- Planiro/Application/Services/PlannerService.cs
+++ Planiro/Application/Services/PlannerService.cs
@@ -1,10 +1,13 @@
 ﻿namespace Planiro.Application.Services;
 using Planiro.Domain.IRepositories;
-public class PlannerService(IPlannerRepository repository)
+public class PlannerService(IPlannerRepository repository, IUserRepository userRepository, 
+    ITeamRepository teamRepository)
 {
     public async Task CreatePlanner(string joinCode, string userName)
     {
         var plannerId = Guid.NewGuid();
-        await repository.CreatePlanner(joinCode, userName, plannerId);
+        var user = await userRepository.GetUserByUserNameAsync(userName);
+        var teamId = await teamRepository.GetTeamIdByJoinCodeAsync(joinCode);
+        await repository.CreatePlanner(user.Id, teamId);
     }
 }
